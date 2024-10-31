0653083f19d56bca9bb6566ac2edcaa54174c9ed
VIL20776 <vil20776@uvg.edu.gt>
2024-10-31 02:45:46 -0600

    Added BMP utilities

---
+++ common/bmp.cpp
@@ -0,0 +1,43 @@
+#include "bmp.h"
+
+#include <fstream>
+#include <cstring>
+
+BMPImage::BMPImage(Color *data, unsigned int width, unsigned int height) {
+    // Header
+    header = {
+        .signature = {'B', 'M'},
+        .fileSize = 54 + (width * height * 3),
+        .reserved = 0,
+        .dataOffset = 54,
+    };
+    // Info header
+    info = {
+        .size = 40,
+        .width = width,
+        .height = height,
+        .planes = 1,
+        .bitsPerPixel = 24,
+        .compression = 0,
+        .imageSize = width * height * 3,
+        .xPixelsPerM = 0,
+        .yPixelsPerM = 0,
+        .colorsUsed = 0,
+        .importantColors = 0
+    };
+
+    pixels = new Color[width * height];
+    std::memcpy(pixels, data, sizeof(Color) * width * height);
+}
+
+BMPImage::~BMPImage() {
+    delete[] pixels;
+}
+
+void BMPImage::save() {
+    std::ofstream of("output.bmp", std::ios_base::binary);
+    of.write((const char*) &header, sizeof(header));
+    of.write((const char*) &info, sizeof(info));
+    of.write((const char*) pixels, info.imageSize);
+    of.close();
+}
---
+++ common/bmp.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstdint>
+
+struct Color
+{
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+};
+
+class BMPImage {
+private:
+
+#pragma pack(push,1)
+struct Header {
+    unsigned char signature [2];
+    uint32_t fileSize;
+    uint32_t reserved;
+    uint32_t dataOffset;
+} header;
+struct Info {
+    uint32_t size;
+    uint32_t width;
+    uint32_t height;
+    uint16_t planes;
+    uint16_t bitsPerPixel;
+    uint32_t compression;
+    uint32_t imageSize;
+    uint32_t xPixelsPerM;
+    uint32_t yPixelsPerM;
+    uint32_t colorsUsed;
+    uint32_t importantColors;
+} info;
+#pragma pack(pop)
+
+Color *pixels;
+
+public:
+BMPImage(Color* data, unsigned int width, unsigned int height);
+~BMPImage();
+
+void save();
+
+};
